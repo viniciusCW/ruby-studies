c72f4f5922e4b611efc29ef2e280b496cd9ccbcf
Vinicius Noronha <viniciusna633@outlook.com>
2022-04-21 23:18:50 -0300

    mexendo com csv

---
+++ annotations/csv/test-csv.rb
@@ -0,0 +1,14 @@
+require 'csv'
+
+#CSV.parse("1,chocolate\n2,bacon\n3,apple")
+#CSV.read(File.join(File.dirname(__FILE__), 'teste.csv'))
+
+list = CSV.read('C:\Users\vinic\Documents\Programação\Ruby\annotations\csv\teste.csv')
+
+list.append([6, "potato"])
+
+listMapped = list.map { |row| row.join(",")}.join("\n")
+
+p listMapped
+
+File.write('C:\Users\vinic\Documents\Programação\Ruby\annotations\csv\teste.csv', listMapped)
